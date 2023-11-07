534136728146dd1f2d524220623ff99829c28524
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-11-07 08:55:25 +0100

    initial commit

--- src/de/demmer/dennis/Aufgabe1.java
+++ src/de/demmer/dennis/Aufgabe1.java
@@ -1,8 +1,27 @@
 package de.demmer.dennis;
 
 public class Aufgabe1 {
+
+	public static void main(String[] args) {
+
+		//Welche Datentypen sind hier am besten anzuwenden?
+		
+		int i = 3.71882;
+		int j = 981675685;
+		int k = 9922457417;
+		int l = "grün";
+		int m = a == b;
+		int n = "19 Prozent";
+		int o = true;
+		int p = 9999199.3;
+		int q = 9283847762773.31223;
+		int r = "13. Januar";
+		int s = 2;
+		int t = 'g';
+
+
 	
-	
-	
+		
+	}
 
 }
